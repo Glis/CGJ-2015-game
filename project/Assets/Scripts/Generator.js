6b80e479d4a8173ab01f54af52f5e5da2b78a2de
Abraham José González Barboza <jgab.net@gmail.com>
2014-01-25 09:29:36 -0430

    added generator partial

---
+++ project/Assets/Scripts/Generator.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+StartCoroutine("Do");
+
+var enemy : GameObject;
+
+
+function Do () {
+
+	while(true){
+		GameObject.Instantiate(enemy,Vector3.zero,Quaternion.identity);
+		
+		yield WaitForSeconds (2);		
+	}
+	
+}
