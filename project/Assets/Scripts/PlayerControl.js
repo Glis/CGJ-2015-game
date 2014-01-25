82b7a360b374bea2711eb4e8b2ef846db22656b6
Abraham José González Barboza <jgab.net@gmail.com>
2014-01-25 08:22:50 -0430

    added player movement

---
+++ project/Assets/Scripts/PlayerControl.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+var moveUp: KeyCode;
+var moveDown: KeyCode;
+var moveLeft: KeyCode;
+var moveRight: KeyCode;
+
+var speed : float = 2;
+
+function Update () {
+
+	if(Input.GetKey(moveUp)){
+		rigidbody2D.velocity.y = speed;
+	}else if(Input.GetKey(moveDown)){
+		rigidbody2D.velocity.y=speed*-1;
+	}else{
+		rigidbody2D.velocity.y=0;
+	}
+	
+	if(Input.GetKey(moveLeft)){
+		rigidbody2D.velocity.x=speed*-1;
+	}else if(Input.GetKey(moveRight)){
+		rigidbody2D.velocity.x=speed;
+	}else{	
+		rigidbody2D.velocity.x=0;
+	}
+
+}
