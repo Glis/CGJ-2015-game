bb0f1c1eddd4de2794f9eaa5c391bbbfdec17bac
Stephs15 <estefi1515@gmail.com>
2014-01-25 19:00:17 -0430

    Animaciones Marjorie

---
+++ project/Assets/Scripts/MarPruebas/LeftToRight.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftToRight : MonoBehaviour {
+
+	public float speed = 1.0f;
+	public string axisName = "Horizontal";
+	public Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetFloat ("Speed", Mathf.Abs(Input.GetAxis(axisName)));
+		transform.position += transform.right * Input.GetAxis(axisName) * speed * Time.deltaTime;
+	}
+}
