0e44c7484e222c6c1a985c04a1d5fdee7bc2dd59
Glis <glisty44@hotmail.com>
2014-01-25 09:43:59 -0430

    pregenerator fixes

--- project/Assets/Scripts/Generator.js
+++ project/Assets/Scripts/Generator.js
@@ -17,9 +17,9 @@ function Do () {
 		if(randy == 0) randy = -1;
 		else randy = 1; 
 		
-		var x : int = Random.Range(5*randx,(5+3)*randx);
-		var y : int = Random.Range(6*randy,(6+3)*randy);
-		GameObject.Instantiate(enemy,new Vector3 ( x, y,0f),Quaternion.identity);
+		var x : int = Random.Range(5*randx,(5+1)*randx);
+		var y : int = Random.Range(5*randy,(5+1)*randy);
+		GameObject.Instantiate(enemy,new Vector3 (x,y,0f),Quaternion.identity);
 		
 		yield WaitForSeconds (0.7);		
 	}
