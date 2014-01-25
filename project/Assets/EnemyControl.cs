7711c16dfe38992fdf910bb16a298da70548b192
Juan8 <gordonfreenan@gmail.com>
2014-01-25 03:52:49 -0430

    basic AI

---
+++ project/Assets/EnemyControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyControl : MonoBehaviour {
+
+	private Transform Target;
+	private float speed = 3.0f;
+	// Use this for initialization
+	void Start () {
+		Target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 director = (Target.position - transform.position).normalized;
+		rigidbody2D.velocity = speed * director;
+	}
+}
