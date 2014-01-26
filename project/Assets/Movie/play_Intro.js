9b3f0604cee90928fac34657642af1b6f94f23c1
Juan8 <gordonfreenan@gmail.com>
2014-01-26 14:33:03 -0430

    videos

---
+++ project/Assets/Movie/play_Intro.js
@@ -0,0 +1,12 @@
+﻿
+
+function Start () {
+	var mov: MovieTexture = renderer.material.mainTexture;
+	mov.Play();
+	yield(WaitForSeconds(mov.duration));
+           Application.LoadLevel("Intro_Loop");
+}
+
+function Update () {
+
+}
