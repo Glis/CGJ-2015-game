c346ad0b87a2656713bad90070d21b38ddb13fe0
Stephs15 <estefi1515@gmail.com>
2014-01-25 20:06:22 -0430

    Pelusa (Enemigo)

--- project/Assets/Scripts/Enemy/EnemyControl.cs
+++ project/Assets/Scripts/Enemy/EnemyControl.cs
@@ -4,23 +4,31 @@ using System.Collections;
 public class EnemyControl : MonoBehaviour {
 
 	private Vector3 Target;
-	private float speed = 3.0f;
+	public float Speed = 20.0f;
+	public Animator anim;
+
 	// Use this for initialization
 	void Start () {
 		StartCoroutine("random_target");
+		anim = gameObject.GetComponent<Animator>();	
 	}
 
 	IEnumerator random_target () {
 		while(true)
 		{
 			Target = new Vector3(Random.Range(-20,20),Random.Range(-20,20),0f);
+
 			yield return new WaitForSeconds(Random.Range(1,3));
 		}
 	}
 
 	// Update is called once per frame
 	void Update () {
+
 		Vector3 director = (Target - transform.position).normalized;
-		rigidbody2D.velocity = speed * director;
+		rigidbody2D.velocity = Speed * director;
+//		print(rigidbody2D.velocity.ToString());
+		anim.SetFloat ("Speed", Mathf.Abs(rigidbody2D.velocity.magnitude));
+
 	}
 }
