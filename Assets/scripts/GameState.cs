a9db07221a3987e7914073336ffd6f7e347b01e7
jfuentes <jfuentes@4geeks.co>
2015-01-24 13:34:52 -0430

    gamestate script created

--- Assets/scripts/GameState.cs
+++ Assets/scripts/GameState.cs
@@ -1,12 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class gamestateScript : MonoBehaviour {
+public class GameState : MonoBehaviour {
 
-	public int speed;
 	// Use this for initialization
 	void Start () {
-		Physics.gravity = new Vector3(0, 0.0F, 0);
+	
 	}
 	
 	// Update is called once per frame
