a2db469dd6f14ce1e0c8d2ebb639d2abf94aa456
jfuentes <jfuentes@4geeks.co>
2015-01-24 04:39:53 -0430

    player being made

---
+++ Assets/scripts/playerScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.AddTorque (Random.Range(-0.5F, 0.5F));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
