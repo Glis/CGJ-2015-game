c2ccb569666f253320aff52f2e39e95e9d70c101
Alejandro Cannizzo <alejandrocannizzo@wattopoststudio.com>
2015-01-25 00:41:12 -0430

    whoops

--- Assets/scripts/BackgroundParallaxer.cs
+++ Assets/scripts/BackgroundParallaxer.cs
@@ -32,11 +32,9 @@ public class BackgroundParallaxer : MonoBehaviour {
 	void Update () 
 	{
 		float x = Mathf.Repeat((Camera.main.transform.position.x%100f) * scrollSpeed * SPEED_MODIFIER, 1);
-		Vector2 offset = new Vector2(Mathf.Max(0f,x),Mathf.Max(0f,savedOffset.y)));
+		Vector2 offset = new Vector2(Mathf.Max(0f,x),Mathf.Max(0f,savedOffset.y));
 		renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
 
-		Debug.Log ("OFSET!" + offset);
-		
 		if(cameraVerticalBounds != null && cameraVerticalBounds.Length == 2)
 		{
 			float y = 1f-Mathf.InverseLerp(cameraVerticalBounds[0], cameraVerticalBounds[1], Camera.main.transform.position.y);
