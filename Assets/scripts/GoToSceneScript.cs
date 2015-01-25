58d99dfc6b4f65e0204a6598e3bb1380c5ec08e5
Alejandro Cannizzo <alejandrocannizzo@wattopoststudio.com>
2015-01-25 14:42:26 -0430

    Added GoToScene script

---
+++ Assets/scripts/GoToSceneScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToSceneScript : MonoBehaviour 
+{
+
+	public string sceneName;
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Application.LoadLevel(sceneName);
+		}
+	}
+}
