5dbebbd732f427e8ad3062585f1776a2593654d4
Alejandro Cannizzo <alejandrocannizzo@wattopoststudio.com>
2015-01-25 14:45:18 -0430

    Added exit on time

--- Assets/scripts/GoToSceneScript.cs
+++ Assets/scripts/GoToSceneScript.cs
@@ -5,7 +5,15 @@ public class GoToSceneScript : MonoBehaviour
 {
 
 	public string sceneName;
-
+	public bool exitAfterTime;
+	public float exitTime;
+	
+	IEnumerator Start()
+	{
+		if(exitAfterTime)
+			yield return new WaitForSeconds(exitTime);
+		yield return null;
+	}
 	
 	// Update is called once per frame
 	void Update () 
