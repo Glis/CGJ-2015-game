a1d2c263a6e368912119612d28c9b98bcefaf0b0
Alejandro Cannizzo <alejandrocannizzo@wattopoststudio.com>
2015-01-25 00:02:34 -0430

    Added vertical parallaxing based on the camera's boundaries

--- Assets/scripts/BackgroundParallaxer.cs
+++ Assets/scripts/BackgroundParallaxer.cs
@@ -5,24 +5,41 @@ using System.Collections;
 public class BackgroundParallaxer : MonoBehaviour {
 
 	public const float SPEED_MODIFIER = 0.01f;
+	public const float VERTICAL_OFFSET_MODIFIER = 0.07f;
 	public float scrollSpeed;
+	
+	private float originalY;
 	private Vector2 savedOffset;
+	private float[] cameraVerticalBounds;
 
 	// Use this for initialization
 	void Start () {
-		float vertExtent = Camera.main.camera.orthographicSize;    
+		originalY = transform.position.y;
 		
-		transform.localScale = new Vector3(2.56f * vertExtent / 1.01f , vertExtent) * 2;
+		cameraFollow followScript = Camera.main.GetComponent<cameraFollow>();
+		if(followScript != null)
+		{
+			cameraVerticalBounds = new float[2];
+			cameraVerticalBounds[0] = followScript.boundaries.position.y - followScript.boundaries.height * 0.5f + followScript.vertExtent;
+			cameraVerticalBounds[1] = followScript.boundaries.position.y + followScript.boundaries.height * 0.5f - followScript.vertExtent;
+		}
+		float vertExtent = Camera.main.camera.orthographicSize;  
+		transform.localScale = new Vector3(2.56f * vertExtent / 1.01f * 1.1f, vertExtent) * 2 * 1.3f;
 		savedOffset = renderer.sharedMaterial.GetTextureOffset("_MainTex");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
-
+	void Update () 
+	{
 		float x = Mathf.Repeat((Camera.main.transform.position.x%100f) * scrollSpeed * SPEED_MODIFIER, 1);
 		Vector2 offset = new Vector2(x,savedOffset.y);
 		renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+		
+		if(cameraVerticalBounds != null && cameraVerticalBounds.Length == 2)
+		{
+			float y = 1f-Mathf.InverseLerp(cameraVerticalBounds[0], cameraVerticalBounds[1], Camera.main.transform.position.y);
+			transform.localPosition = new Vector3(0, y * scrollSpeed * VERTICAL_OFFSET_MODIFIER);
+		}
 	}
 	
 	void OnDisable()
