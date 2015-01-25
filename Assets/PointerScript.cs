a7eb1f6fdcaebfdf5bc9f272f97c21eab4c68726
Alejandro Cannizzo <alejandrocannizzo@wattopoststudio.com>
2015-01-25 12:58:58 -0430

    Added pointer prefab

---
+++ Assets/PointerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointerScript : MonoBehaviour {
+
+	private Transform machine;
+
+	// Use this for initialization
+	void Start () 
+	{
+		machine = GameObject.FindWithTag("Goal").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.LookAt(machine);
+		transform.Rotate(new Vector3(0, 90, 90));
+	}
+}
